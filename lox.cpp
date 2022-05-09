fb880589b38147f1b064fe5d3af013be08977116
Alexander Dumais <alexanderddumais@gmail.com>
2022-05-08 23:09:04 -0600

    Fixed line I/O. Ready to implement Scanner.

--- lox.cpp
+++ lox.cpp
@@ -3,41 +3,12 @@
 #include <fstream>
 
 using namespace std;
+#define DEBUG_ON 1
 
-void println(string s)
-{
-    cout << s << endl;
-}
-
-void runFile(char *filename)
-{
-    std::ifstream file;
-    file.exceptions(std::ifstream::badbit | std::ifstream::failbit);
-    //char *chars;
-
-    try{
-        file.open(filename);
-        while (!file.eof()) {
-            //char(file.get()) >> chars;
-            cout << char(file.get()) << endl;
-        }
-        file.close();
-        
-    }
-    catch (std::ifstream::failure e) {
-        std::cerr << "Exception opening, reading or closing file." << std::endl;
-    }
-
-    //cout << chars << endl;
-
-    println("##########TODO: Implement runFile().");
-    println(filename);
-}
-
-void runPrompt()
-{
-    println("##########TODO: Implement runPrompt()");
-}
+void println(string s);
+void DEBUG(string s);
+void runFile(char *filename);
+void runPrompt();
 
 int main(int argc, char **argv)
 {
@@ -52,19 +23,58 @@ int main(int argc, char **argv)
     }
     else
     {
+        DEBUG( "Entering runPrompt()\n" );
         runPrompt();
     }
 }
 
-/*
-package com.craftinginterpreters.lox;
+void println(string s)
+{
+    cout << s << endl;
+}
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+void DEBUG(string s)
+{
+    if (DEBUG_ON)
+    {
+        cout << "DEBUG: " << s << endl;
+    }
+}
 
-*/
+void run(string source) {
+    
+}
+void runFile(char *filename)
+{
+    std::ifstream file;
+    file.exceptions(std::ifstream::badbit);
+
+    try
+    {
+        file.open(filename);
+        string line;
+        while (getline(file, line))
+        {
+            DEBUG(line);
+            // run(line);
+        }
+        file.close();
+    }
+    catch (std::ifstream::failure e)
+    {
+        std::cerr << "Exception opening, reading or closing file." << std::endl;
+    }
+
+    DEBUG("TODO: Implement runFile().");
+}
+
+void runPrompt()
+{
+    string line;
+    while(getline(std::cin, line)) {
+        DEBUG(line);
+        //run(line);
+    }
+
+    DEBUG("TODO: Implement runPrompt()");
+}
